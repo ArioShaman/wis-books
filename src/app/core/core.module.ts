7f9665a36be3493a8ae5d770dec2708d8bd51ef4
ArioShaman <ariosha6@gmail.com>
2020-08-21 16:01:37 +0300

    fixed responses of pull request

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,11 +10,14 @@ import { AppRoutingModule } from '../app-routing.module';
 
 import { AuthorsService } from './services/authors.service';
 import { GenresService } from './services/genres.service';
+import { DomainInterceptor } from './interceptors/domain.interceptor';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    HttpClientModule,
+
     MatSidenavModule,
     MatButtonModule,
     MatIconModule,
@@ -21,7 +25,9 @@ import { GenresService } from './services/genres.service';
     AppRoutingModule,
   ],
   exports: [
+    HttpClientModule,
     CommonModule,
+
     MatSidenavModule,
     MatButtonModule,
     MatIconModule,
@@ -31,6 +37,11 @@ import { GenresService } from './services/genres.service';
   providers: [
     AuthorsService,
     GenresService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: DomainInterceptor,
+      multi: true,
+    },
   ],
 })
 export class CoreModule { }
--- src/app/core/services/genres.service.ts
+++ src/app/core/services/genres.service.ts
@@ -4,31 +4,30 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { environment } from '../../../environments/environment';
 import { Genre } from '../../genres/models/genre.model';
+
+interface IResponse {
+  genres: Genre[];
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class GenresService {
 
-  protected API_URL: string = environment.hosts.api_host;
-
   constructor(
     private http: HttpClient,
   ) { }
 
   public getAllGenres(): Observable<Genre[]> {
-    return this.http.get(
-      this.API_URL + '/genres',
-      {
-        params: new HttpParams().set('limit', '30'),
-      },
-    ).pipe(
-      map((res: any) => {
-        return Genre.newCollection(Genre, res.genres);
-      }),
-    );
+    const params = {
+      params: new HttpParams().set('limit', '30'),
+    };
+
+    return this.http.get<IResponse>('/genres', params)
+      .pipe(
+        map(res => Genre.newCollection(Genre, res.genres)),
+      );
   }
 
 }
