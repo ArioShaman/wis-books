dad7f398b563fa8e4b3b6ccb7906b7d3081e4cc5
ArioShaman <ariosha6@gmail.com>
2020-08-21 16:28:23 +0300

    deleted spec files

--- src/app/books/pipes/filter-by-genre.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterByGenrePipe } from './filter-by-genre.pipe';
-
-describe('FilterByGenrePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterByGenrePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
